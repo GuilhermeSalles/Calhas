e6480f9340764901b4b2805d8f1f7d61d2645f17
GuilhermeSalles <guibaltazarvs@gmail.com>
2024-06-26 15:43:15 -0300

    enviar email inbox ok

--- enviar_email.php
+++ enviar_email.php
@@ -1,36 +1,43 @@
 <?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; // Inclui o autoload do Composer
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recupera os dados do formulário
     $email = $_POST["email"];
     $assunto = $_POST["assunto"];
     $mensagem = $_POST["mensagem"];
 
-    // E-mail de destino
-    $to = 'vendas@calhastaquaral.com.br';
+    // Configurações de SMTP
+    $mail = new PHPMailer(true);
+    try {
+        // Configuração do servidor SMTP
+        $mail->isSMTP();
+        $mail->Host = 'smtp.hostinger.com'; // Configure com o seu host SMTP
+        $mail->SMTPAuth = true;
+        $mail->Username = 'vendas@calhastaquaral.com.br'; // Insira seu e-mail aqui
+        $mail->Password = 'Vendas.123'; // Insira sua senha aqui
+        $mail->SMTPSecure = 'ssl'; // tls se preferir usar tls
+        $mail->Port = 465; // 587 se preferir usar tls
 
-    // Assunto do e-mail
-    $subject = $assunto;
+        // Remetente e destinatário
+        $mail->setFrom('vendas@calhastaquaral.com.br'); // Use um e-mail válido configurado na Hostinger
+        $mail->addAddress('vendas@calhastaquaral.com.br');
 
-    // Construir a mensagem
-    $message = "E-mail enviado por: $email\n\n";
-    $message .= "Mensagem:\n$mensagem";
+        // Conteúdo do e-mail
+        $mail->isHTML(false); // Define se o e-mail será enviado como HTML ou texto simples
+        $mail->Subject = $assunto; // Define o assunto do e-mail
+        $mail->Body = "E-mail enviado por: $email\n\nMensagem:\n$mensagem"; // Define o corpo do e-mail
 
-    // Cabeçalhos do e-mail
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/plain; charset=utf-8\r\n";
-
-    // Configurações do servidor SMTP
-    ini_set('SMTP', 'smtp.hostinger.com');
-    ini_set('smtp_port', 465);
-    ini_set('sendmail_from', 'vendas@calhastaquaral.com.br');
-
-    // Enviar e-mail
-    if (mail($to, $subject, $message, $headers)) {
+        // Envia o e-mail
+        $mail->send();
         echo '<script>alert("Mensagem enviada com sucesso!"); window.location.href = "../";</script>';
-    } else {
+    } catch (Exception $e) {
         echo '<script>alert("Erro ao enviar mensagem. Por favor, tente novamente mais tarde."); window.location.href = "../";</script>';
+        // Descomente a linha abaixo para obter mais detalhes sobre o erro
+        echo 'Erro no envio do email: ', $mail->ErrorInfo;
     }
 }
-?>
